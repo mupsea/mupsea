6e23955a28ea0837128ca5f9af2cb9c905210605
mupsea <69822114+mupsea@users.noreply.github.com>
2023-04-21 20:05:18 +0100

    Update Into.sh

--- Into.sh
+++ Into.sh
@@ -1,1 +1,21 @@
+apt update
+apt upgrade -y
+pkg update
+pkg upgrade -y
+pkg install git -y
+pkg install python -y
+pkg install python2 -y
+pkg install wget -y
+pkg install curl -y
+pkg install perl -y
+pkg install openssl -y
+pkg install php -y
+pip install lolcat
+pip install request
+cd
+clear
+chomod +x *
+pkg install kona -y
+
+
 
